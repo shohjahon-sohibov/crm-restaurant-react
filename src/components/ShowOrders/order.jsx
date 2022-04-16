77c9022fa84aa9bf58d5d94180c00e9e0dabd2ea
shohjahon-sohibov <sohibov.s@bk.ru>
2022-04-16 18:25:26 +0500

    delete numbers of orders in table

--- src/components/ShowOrders/order.jsx
+++ src/components/ShowOrders/order.jsx
@@ -1,5 +1,4 @@
 import { useQuery } from "@apollo/client";
-import { useState } from "react";
 import { ALL_ORDERS } from "../../Query";
 
 const Order = () => {
@@ -14,7 +13,6 @@ const Order = () => {
             <tr>
               <th scope="col">#</th>
               <th scope="col">Name</th>
-              <th scope="col">Number</th>
               <th scope="col">Price</th>
               <th scope="col">location</th>
               <th scope="col">OrderedAt</th>
@@ -28,7 +26,6 @@ const Order = () => {
                 <tr key={i}>
                   <td>{e.id}</td>
                   <th scope="row">{e.name}</th>
-                  <td>{e.number}</td>
                   <td>{e.price}</td>
                   <td>{e.location}</td>
                   <td>{e.createdAt}</td>
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client"; 
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
+  uri: "https://restaurant-web-site-1.herokuapp.com/graphql",
   cache: new InMemoryCache(),
 });
 
